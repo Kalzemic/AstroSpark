3054c7bb66def1c9a1e0af9a6d5c7375b8dd2c37
Kalzemic <Mizizov@gmail.com>
2026-02-25 15:27:27 +0200

    more type definition

--- optimizer/QueryOptimizer.py
+++ optimizer/QueryOptimizer.py
@@ -4,5 +4,5 @@ from queryparser.adql import ADQLQueryTranslator
 class QueryOptimizer():
 
     def __call__(self, adt:ADQLQueryTranslator)-> list[str]:
-            
+        
         return [' ']
