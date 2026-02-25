65bf1ea151aa7fc658914a75f6a5feb96fe19fb6
Kalzemic <Mizizov@gmail.com>
2026-02-25 15:58:39 +0200

    create search

--- SparkEngine/engine.py
+++ SparkEngine/engine.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import DataFrame
+from queryparser.adql import ADQLQueryTranslator
 
 class SparkEngine():
     def __init__(self, session: SparkSession):
@@ -7,6 +8,12 @@ class SparkEngine():
         self.session = session
         
 
-    def search(self, urls: list[str])-> DataFrame:
+    def search(self, adt:ADQLQueryTranslator, urls: list[str]):
 
-        return self.session.read.parquet(*urls)
+        df = self.session.read.parquet(*urls)
+        sql_query = adt.to_postgresql()
+
+        df.createOrReplaceTempView("gaiadr3.gaia_source")
+        result_df = self.session.sql(sql_query)
+        result_df.show()
+
